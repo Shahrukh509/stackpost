3214d44f8ca11a83cdc530a9735bae3de7914fbe
Shahrukh509 <62711316+Shahrukh509@users.noreply.github.com>
2022-10-20 19:59:16 +0500

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'stackpost');
+define('TIMEZONE', 'Asia/Kolkata');
+define('ENCRYPTION_KEY', '16e1290fd161c63812e9f786ca7684ca');
